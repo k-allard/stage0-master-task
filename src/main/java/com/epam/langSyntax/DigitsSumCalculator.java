a19a450987a9e9fb2d1284113abf84c604b98090
kallard <kallard@student.21-school.ru>
2023-04-12 14:20:22 +0400

    Done OOP, LangSyntax and Conditions (1-4) Master tasks

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,14 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int result = 0;
+        int currentDigit;
+        while (number > 0) {
+            currentDigit = number % 10;
+            result += currentDigit;
+            number = number / 10;
+        }
+        System.out.println(result);
     }
 
 }
