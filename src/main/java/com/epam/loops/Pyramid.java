f01c0f3176b713db19583fcd64c0f64e08aba70f
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-04-12 15:41:49 +0400

    Done Loops master tasks

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,11 +3,20 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cathetusLength; i++) {
+            sb.append(" ".repeat(Math.max(0, cathetusLength - 1 - i)));
+            for (int k = i + 1; k >= 2; k--)
+                sb.append(k);
+            for (int f = 1; f <= i + 1; f++)
+                sb.append(f);
+            sb.append("\n");
+        }
+        System.out.print(sb);
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(6);
     }
 
 }
